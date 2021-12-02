0a1fbcce51e0721a5f77bc674abda6b29104c9b7
anonymous <89799912+PRATHEEsni@users.noreply.github.com>
2021-12-03 00:55:01 +0530

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -66,8 +66,8 @@ def changeImageSize(maxWidth, maxHeight, image):
     heightRatio = maxHeight / image.size[1]
     newWidth = int(widthRatio * image.size[0])
     newHeight = int(heightRatio * image.size[1])
-    return image.resize((newWidth, newHeight))
-
+    newImage = image.resize((newWidth, newHeight))
+    return newImage
 
 async def generate_cover(requested_by, title, views, duration, thumbnail):
     async with aiohttp.ClientSession() as session:
@@ -78,7 +78,7 @@ async def generate_cover(requested_by, title, views, duration, thumbnail):
                 await f.close()
 
     image1 = Image.open("./background.png")
-    image2 = Image.open("etc/LightBlue.png")
+    image2 = Image.open("etc/foreground.png")
     image3 = changeImageSize(1280, 720, image1)
     image4 = changeImageSize(1280, 720, image2)
     image5 = image3.convert("RGBA")
@@ -86,13 +86,14 @@ async def generate_cover(requested_by, title, views, duration, thumbnail):
     Image.alpha_composite(image5, image6).save("temp.png")
     img = Image.open("temp.png")
     draw = ImageDraw.Draw(img)
-    font = ImageFont.truetype("etc/font.otf", 32)
-    draw.text((190, 550), f"Title: {title[:70]} ...", (255, 255, 255), font=font)
-    draw.text((190, 590), f"Duration: {duration}", (255, 255, 255), font=font)
-    draw.text((190, 630), f"Views: {views}", (255, 255, 255), font=font)
+    font = ImageFont.truetype("etc/regular.ttf", 32)
+    draw.text((205, 550), f"Title: {title}", (51, 215, 255), font=font)
     draw.text(
-        (190, 670),
-        f"Powered By:「 𝙋𝙍𝘼𝙏𝙃𝙀𝙀𝙆 」→🇮🇳",
+        (205, 590), f"Duration: {duration}", (255, 255, 255), font=font
+    )
+    draw.text((205, 630), f"Views: {views}", (255, 255, 255), font=font)
+    draw.text((205, 670),
+        f"Added By: {requested_by}",
         (255, 255, 255),
         font=font,
     )
